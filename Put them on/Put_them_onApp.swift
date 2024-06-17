1638d78c4151130028762817a505172ac5f266d1
DanielJunsangCho <junc13579@gmail.com>
2024-06-16 20:09:43 -0400

    Initial Commit

---
+++ Put them on/Put_them_onApp.swift
@@ -0,0 +1,32 @@
+//
+//  Put_them_onApp.swift
+//  Put them on
+//
+//  Created by Daniel Cho on 6/16/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Put_them_onApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
